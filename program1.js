0244960ef41ff09894ebd2c527844f42c0af445c
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-04-29 12:49:08 +0400

    While, Do While

---
+++ program1.js
@@ -0,0 +1,16 @@
+//while
+let a = [2, 5, 4, 8, 5, 4];
+let i = 0;
+while (i < a.length) {
+  console.log("for", i, a[i]);
+  i++;
+}
+
+
+//do while
+let a = [2, 5, 4, 8, 5, 4];
+let i = 0;
+do {
+  console.log("for", i, a[i]);
+  i++;
+} while (i < a.length);
