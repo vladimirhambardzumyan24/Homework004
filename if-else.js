bd23c2a1a287e752718a7b297e200922540af175
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-04-30 12:15:40 +0400

    All if-else tasks

---
+++ if-else.js
@@ -0,0 +1,42 @@
+// if ("0") {
+//     console.log( 'Hello' );
+//   }
+
+
+
+
+// let str = "ECMAScripts";
+
+// if (str == 'ECMAScript') {
+//   console.log('Right!');
+// } else {
+//   console.log("You don't know? ECMAScript!");
+// }
+
+
+
+// let number = 0
+
+// if (number > 0) {
+//   console.log( 1 );
+// } else if (number < 0) {
+//     console.log( -1 );
+// } else {
+//     console.log( 0 );
+// }
+
+
+
+
+// let result = (a + b < 4) ? 'Below' : 'Over';
+
+
+
+
+
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
+
+
