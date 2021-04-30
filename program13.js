1619f51e891eb30c97778fca93076f0bfee7ea19
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-04-30 13:23:39 +0400

    Revers number

--- program13.js
+++ program13.js
@@ -1,7 +1,5 @@
-let number=4564605
-let num=number.toString().split('')
-if (num[num.length-1]==0) {
-    console.log(number)
-}else{
-    console.log(number.toString().split('').reverse().join(''))
-}
+let num=-789
+num=Math.abs(num)
+let firstNum=num.toString().substring(0, num.toString().length - 1)
+let lastNum=num.toString().split("").pop()+firstNum
+console.log(lastNum)
