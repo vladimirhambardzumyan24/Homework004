eab04af25278017735ce2c1b8897e18a61c1bcf1
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-04-30 13:24:55 +0400

    All while-for tasks

---
+++ while-for.js
@@ -0,0 +1,63 @@
+// let i = 3;
+// while (i) {
+//   console.log( i-- );
+// }
+// 3,2,1
+
+
+
+
+// let i = 0;
+// while (++i < 5) console.log( i );
+
+// let i = 0;
+// while (i++ < 5) console.log( i );
+
+
+
+// for (let i = 0; i < 5; i++) console.log( i );
+
+// for (let i = 0; i < 5; ++i) console.log( i );
+
+
+
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//       console.log( i );
+//     }
+//   }
+
+
+
+// let i = 0;
+// while (i < 3) {
+//   console.log( `number ${i}!` );
+//   i++;
+// }
+
+
+
+// let num;
+
+// do {
+//   num = 55;
+// } while (num <= 100 && num);
+// console.log(num)
+
+
+
+
+// let n = 10;
+
+//
+// for (let i = 2; i <= n; i++) { 
+
+//   for (let j = 2; j < i; j++) { 
+//     if (i % j == 0) continue ; 
+//   }
+
+//   console.log( i );
+// }
+
+
